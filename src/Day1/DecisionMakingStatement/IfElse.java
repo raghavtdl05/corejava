3e59dc2baaf0ccae61f8e6f85a32585c0929a2f1
Raghav-2025 <baghelraghav818@gmail.com>
2025-09-19 22:53:29 +0530

    DecisionMaking

---
+++ src/Day1/DecisionMakingStatement/IfElse.java
@@ -0,0 +1,21 @@
+package Day1.DecisionMakingStatement;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 12;
+        if (x + y > 20) {
+
+// if(condition)
+//
+//    {
+//        statement 1; //executes when condition is true
+//    }
+//else
+//
+//    {
+//        statement 2; //executes when condition is false
+//    }
+        }
+    }
+}
