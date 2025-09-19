1a5daaf8f443827553f0364ec98cf18f62e53a38
Raghav-2025 <baghelraghav818@gmail.com>
2025-09-19 22:26:52 +0530

    Multiplication

---
+++ src/Day1/Multiplication.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Multiplication {
+    public static void main(String[] args) {
+
+
+        int a = 5;
+        int b = 15;
+        int c = a * b;
+        System.out.println("Multiplication " +c);
+
+    }
+}
