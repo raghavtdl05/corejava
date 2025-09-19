366c77b20d2c6984a3560f1d975dc7727cd94553
Raghav-2025 <baghelraghav818@gmail.com>
2025-09-19 21:59:38 +0530

    first commit

---
+++ src/Day1/Addition.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Addition {
+    public static void main(String[] args) {
+
+
+        int a = 12;
+        int b = 34;
+        int c = a + b;
+        System.out.println("Addition " + c);
+
+
+    }
+}
---
+++ src/Day1/Demo.java
@@ -0,0 +1,9 @@
+package Day1;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Word" );
+    }
+}
